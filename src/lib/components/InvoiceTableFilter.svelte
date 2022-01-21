7743a47aba5b9982ea01e296434fa9da4b32507c
Levi Bassey <programlevi@gmail.com>
2022-01-21 22:17:05 +0100

    Toggle invoice filter onclick

--- src/lib/components/InvoiceTableFilter.svelte
+++ src/lib/components/InvoiceTableFilter.svelte
@@ -1,9 +1,31 @@
-<div class="relative ml-auto">
-  <button class="flex items-center">
+<script lang="ts">
+  import { afterUpdate, onDestroy } from "svelte";
+  import { browser } from "$app/env";
+
+  let isOpen = false;
+
+  const close = () => (isOpen = false);
+
+  afterUpdate(() => {
+    if (isOpen) {
+      document.addEventListener("click", close);
+    }
+  });
+
+  onDestroy(() => {
+    if (browser) {
+      document.removeEventListener("click", close);
+    }
+  });
+</script>
+
+<div on:click|stopPropagation class="relative ml-auto">
+  <button class="flex items-center" on:click={() => (isOpen = !isOpen)}>
     <span class="font-bold md:hidden">Filter</span>
     <span class="font-bold hidden md:block"> Filter by status </span>
     <svg
       class="ml-3 w-2.5 h-2 stroke-indigo-200 stroke-2 md:ml-4"
+      class:rotate-180={isOpen}
       viewBox="0 0 10 7"
       fill="none"
     >
@@ -11,30 +33,32 @@
     </svg>
   </button>
 
-  <div
-    class="absolute top-8 right-1/2 translate-x-1/2 w-48 rounded-lg p-6 shadow-lg bg-white"
-  >
-    <ul class="space-y-4">
-      <li>
-        <label class="flex items-center">
-          <input
-            type="checkbox"
-            name="status"
-            class="border-none rounded-sm bg-light-blue-200 checked:text-indigo-200"
-          />
-          <span class="ml-3.5 font-bold">Pending</span>
-        </label>
-      </li>
-      <li>
-        <label class="flex items-center">
-          <input
-            type="checkbox"
-            name="status"
-            class="border-none rounded-sm bg-light-blue-200 checked:text-indigo-200"
-          />
-          <span class="ml-3.5 font-bold">Paid</span>
-        </label>
-      </li>
-    </ul>
-  </div>
+  {#if isOpen}
+    <div
+      class="absolute top-8 right-1/2 translate-x-1/2 w-48 rounded-lg p-6 shadow-lg bg-white"
+    >
+      <ul class="space-y-4">
+        <li>
+          <label class="flex items-center">
+            <input
+              type="checkbox"
+              name="status"
+              class="border-none rounded-sm bg-light-blue-200 checked:text-indigo-200"
+            />
+            <span class="ml-3.5 font-bold">Pending</span>
+          </label>
+        </li>
+        <li>
+          <label class="flex items-center">
+            <input
+              type="checkbox"
+              name="status"
+              class="border-none rounded-sm bg-light-blue-200 checked:text-indigo-200"
+            />
+            <span class="ml-3.5 font-bold">Paid</span>
+          </label>
+        </li>
+      </ul>
+    </div>
+  {/if}
 </div>
